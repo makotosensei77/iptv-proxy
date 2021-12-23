b378d3f0f714afbad901bc7b9ca8ad04628bca17
Fatih Ka <xybydy@gmail.com>
2021-12-23 16:27:40 +0300

    init

---
+++ pkg/server/server.go
@@ -0,0 +1,279 @@
+/*
+ * Iptv-Proxy is a project to proxyfie an m3u file and to proxyfie an Xtream iptv service (client API).
+ * Copyright (C) 2020  Pierre-Emmanuel Jacquier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package server
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-contrib/cors"
+	"github.com/jamesnetherton/m3u"
+	uuid "github.com/satori/go.uuid"
+	"github.com/xybydy/iptv-proxy/pkg/config"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
+)
+
+var defaultProxyfiedM3UPath = filepath.Join(os.TempDir(), uuid.NewV4().String()+".iptv-proxy.m3u")
+var endpointAntiColision = strings.Split(uuid.NewV4().String(), "-")[0]
+
+// Mapping structure to help replacing original values over the air
+type Mapping []struct {
+	// Valid key options: name, tvg-name, tvg-id, tvg-logo, group-title
+	Key  string `yaml:"key"`
+	Name string `yaml:"name"`
+	Want string `yaml:"want"`
+}
+
+func (m Mapping) Get(key, name string) string {
+	for _, i := range m {
+		if compareStrings(i.Key, key) && compareStrings(i.Name, name) {
+			return i.Want
+		}
+	}
+	return ""
+}
+
+// Config represent the server configuration
+type Config struct {
+	*config.ProxyConfig
+
+	// M3U service part
+	playlist *m3u.Playlist
+	// this variable is set only for m3u proxy endpoints
+	track *m3u.Track
+	// path to the proxyfied m3u file
+	proxyfiedM3UPath string
+
+	endpointAntiColision string
+
+	mapping Mapping
+}
+
+// NewServer initialize a new server configuration
+func NewServer(config *config.ProxyConfig) (*Config, error) {
+	var p m3u.Playlist
+	var m3uPath string
+
+	switch {
+	case config.RemoteURL.String() != "":
+		m3uPath = config.RemoteURL.String()
+	case config.M3UFileName != "":
+		m3uPath = config.M3UFileName
+	case config.XtreamBaseURL == "":
+		log.Panic("no m3u file or remote url")
+	}
+
+	var err error
+	p, err = m3u.Parse(m3uPath)
+	if err != nil {
+		return nil, err
+	}
+
+	mapping := new(Mapping)
+	if config.MappingPath != "" {
+		var err error
+
+		mapping, err = getMapping(config.MappingPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &Config{
+		config,
+		&p,
+		nil,
+		defaultProxyfiedM3UPath,
+		endpointAntiColision,
+		*mapping,
+	}, nil
+}
+
+// Serve the iptv-proxy api
+func (c *Config) Serve() error {
+	if err := c.playlistInitialization(); err != nil {
+		return err
+	}
+
+	router := gin.Default()
+	router.Use(cors.Default())
+	group := router.Group("/")
+	c.routes(group)
+
+	return router.Run(fmt.Sprintf("%s:%d", c.HostConfig.Hostname, c.HostConfig.Port))
+}
+
+func (c *Config) playlistInitialization() error {
+	if len(c.playlist.Tracks) == 0 {
+		return nil
+	}
+
+	f, err := os.Create(c.proxyfiedM3UPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return c.marshallInto(f, false)
+}
+
+// MarshallInto a *bufio.Writer a Playlist.
+func (c *Config) marshallInto(into *os.File, xtream bool) error {
+	filteredTrack := make([]m3u.Track, 0, len(c.playlist.Tracks))
+
+	ret := 0
+	into.WriteString("#EXTM3U\n") // nolint: errcheck
+	for i, track := range c.playlist.Tracks {
+		var buffer bytes.Buffer
+
+		buffer.WriteString("#EXTINF:")                       // nolint: errcheck
+		buffer.WriteString(fmt.Sprintf("%d ", track.Length)) // nolint: errcheck
+
+		c.updateValues(&track)
+
+		for i := range track.Tags {
+			if i == len(track.Tags)-1 {
+				buffer.WriteString(fmt.Sprintf("%s=%q", track.Tags[i].Name, track.Tags[i].Value)) // nolint: errcheck
+				continue
+			}
+			buffer.WriteString(fmt.Sprintf("%s=%q ", track.Tags[i].Name, track.Tags[i].Value)) // nolint: errcheck
+		}
+
+		uri, err := c.replaceURL(track.URI, i-ret, xtream)
+		if err != nil {
+			ret++
+			log.Printf("ERROR: track: %s: %s", track.Name, err)
+			continue
+		}
+
+		into.WriteString(fmt.Sprintf("%s, %s\n%s\n", buffer.String(), track.Name, uri)) // nolint: errcheck
+
+		filteredTrack = append(filteredTrack, track)
+	}
+	c.playlist.Tracks = filteredTrack
+
+	return into.Sync()
+}
+
+func (c *Config) updateValues(track *m3u.Track) {
+
+	// updating existing tags
+	for i := range track.Tags {
+		newValue := c.mapping.Get(track.Tags[i].Name, track.Name)
+		if newValue != "" {
+			track.Tags[i].Value = newValue
+		}
+	}
+
+	// adding missing tags, searching by old name
+	for _, s := range []string{"tvg-name", "tvg-id", "tvg-logo", "group-title"} {
+		newValue := c.mapping.Get(s, track.Name)
+
+		if newValue != "" {
+			track.Tags = append(track.Tags, m3u.Tag{
+				Name:  "tvg-id",
+				Value: newValue,
+			})
+		}
+	}
+
+	// updating the name
+	newName := c.mapping.Get("name", track.Name)
+	if newName != "" {
+		track.Name = newName
+	}
+
+}
+
+// ReplaceURL replace original playlist url by proxy url
+func (c *Config) replaceURL(uri string, trackIndex int, xtream bool) (string, error) {
+	oriURL, err := url.Parse(uri)
+	if err != nil {
+		return "", err
+	}
+
+	protocol := "http"
+	if c.HTTPS {
+		protocol = "https"
+	}
+
+	customEnd := strings.Trim(c.CustomEndpoint, "/")
+	if customEnd != "" {
+		customEnd = fmt.Sprintf("/%s", customEnd)
+	}
+
+	uriPath := oriURL.EscapedPath()
+	if xtream {
+		uriPath = strings.ReplaceAll(uriPath, c.XtreamUser.PathEscape(), c.User.PathEscape())
+		uriPath = strings.ReplaceAll(uriPath, c.XtreamPassword.PathEscape(), c.Password.PathEscape())
+	} else {
+		uriPath = path.Join("/", c.endpointAntiColision, c.User.PathEscape(), c.Password.PathEscape(), fmt.Sprintf("%d", trackIndex), path.Base(uriPath))
+	}
+
+	basicAuth := oriURL.User.String()
+	if basicAuth != "" {
+		basicAuth += "@"
+	}
+
+	newURI := fmt.Sprintf(
+		"%s://%s%s:%d%s%s",
+		protocol,
+		basicAuth,
+		c.HostConfig.Hostname,
+		c.AdvertisedPort,
+		customEnd,
+		uriPath,
+	)
+
+	newURL, err := url.Parse(newURI)
+	if err != nil {
+		return "", err
+	}
+
+	return newURL.String(), nil
+}
+
+func getMapping(fpath string) (*Mapping, error) {
+	if _, err := os.Stat(fpath); !os.IsNotExist(err) {
+		b, err := os.ReadFile(fpath)
+		if err != nil {
+			return nil, err
+		}
+
+		mapping := new(Mapping)
+		err = yaml.Unmarshal(b, &mapping)
+		if err != nil {
+			log.Print(err)
+		}
+		return mapping, nil
+	}
+	return nil, os.ErrNotExist
+}
+
+func compareStrings(s1, s2 string) bool {
+	return strings.TrimSpace(strings.ToLower(s1)) == strings.TrimSpace(strings.ToLower(s2))
+}
---
+++ pkg/server/xtreamHandles.go
@@ -0,0 +1,419 @@
+/*
+ * Iptv-Proxy is a project to proxyfie an m3u file and to proxyfie an Xtream iptv service (client API).
+ * Copyright (C) 2020  Pierre-Emmanuel Jacquier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package server
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jamesnetherton/m3u"
+	uuid "github.com/satori/go.uuid"
+	xtreamapi "github.com/xybydy/iptv-proxy/pkg/xtream-proxy"
+)
+
+type cacheMeta struct {
+	string
+	time.Time
+}
+
+var hlsChannelsRedirectURL = map[string]url.URL{}
+var hlsChannelsRedirectURLLock = sync.RWMutex{}
+
+// XXX Use key/value storage e.g: etcd, redis...
+// and remove that dirty globals
+var xtreamM3uCache = map[string]cacheMeta{}
+var xtreamM3uCacheLock = sync.RWMutex{}
+
+func (c *Config) cacheXtreamM3u(m3uURL fmt.Stringer) error {
+	xtreamM3uCacheLock.Lock()
+	defer xtreamM3uCacheLock.Unlock()
+
+	playlist, err := m3u.Parse(m3uURL.String())
+	if err != nil {
+		return err
+	}
+
+	tmp := *c
+	tmp.playlist = &playlist
+
+	path := filepath.Join(os.TempDir(), uuid.NewV4().String()+".iptv-proxy.m3u")
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if err := tmp.marshallInto(f, true); err != nil {
+		return err
+	}
+	xtreamM3uCache[m3uURL.String()] = cacheMeta{path, time.Now()}
+
+	return nil
+}
+
+func (c *Config) xtreamGetAuto(ctx *gin.Context) {
+	newQuery := ctx.Request.URL.Query()
+	q := c.RemoteURL.Query()
+	for k, v := range q {
+		if k == "username" || k == "password" {
+			continue
+		}
+
+		newQuery.Add(k, strings.Join(v, ","))
+	}
+	ctx.Request.URL.RawQuery = newQuery.Encode()
+
+	c.xtreamGet(ctx)
+}
+
+func (c *Config) xtreamGet(ctx *gin.Context) {
+	rawURL := fmt.Sprintf("%s/get.php?username=%s&password=%s", c.XtreamBaseURL, c.XtreamUser, c.XtreamPassword)
+
+	q := ctx.Request.URL.Query()
+
+	for k, v := range q {
+		if k == "username" || k == "password" {
+			continue
+		}
+
+		rawURL = fmt.Sprintf("%s&%s=%s", rawURL, k, strings.Join(v, ","))
+	}
+
+	m3uURL, err := url.Parse(rawURL)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	xtreamM3uCacheLock.RLock()
+	meta, ok := xtreamM3uCache[m3uURL.String()]
+	d := time.Since(meta.Time)
+	if !ok || d.Hours() >= float64(c.M3UCacheExpiration) {
+		log.Printf("[iptv-proxy] %v | %s | xtream cache m3u file\n", time.Now().Format("2006/01/02 - 15:04:05"), ctx.ClientIP())
+		xtreamM3uCacheLock.RUnlock()
+		if err := c.cacheXtreamM3u(m3uURL); err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+			return
+		}
+	} else {
+		xtreamM3uCacheLock.RUnlock()
+	}
+
+	ctx.Header("Content-Disposition", fmt.Sprintf(`attachment; filename=%q`, c.M3UFileName))
+	xtreamM3uCacheLock.RLock()
+	path := xtreamM3uCache[m3uURL.String()].string
+	xtreamM3uCacheLock.RUnlock()
+	ctx.Header("Content-Type", "application/octet-stream")
+
+	ctx.File(path)
+}
+
+func (c *Config) xtreamPlayerAPIGET(ctx *gin.Context) {
+	c.xtreamPlayerAPI(ctx, ctx.Request.URL.Query())
+}
+
+func (c *Config) xtreamPanelAPIGET(ctx *gin.Context) {
+	client, err := xtreamapi.New(c.XtreamUser.String(), c.XtreamPassword.String(), c.XtreamBaseURL, ctx.Request.UserAgent())
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	resp, httpcode, err := client.Action(c.ProxyConfig, "get_panel", nil)
+	if err != nil {
+		ctx.AbortWithError(httpcode, err) // nolint: errcheck
+		return
+	}
+	ctx.JSON(http.StatusOK, resp)
+}
+
+func (c *Config) xtreamPlayerAPIPOST(ctx *gin.Context) {
+	contents, err := ioutil.ReadAll(ctx.Request.Body)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	q, err := url.ParseQuery(string(contents))
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.xtreamPlayerAPI(ctx, q)
+}
+
+func (c *Config) xtreamPlayerAPI(ctx *gin.Context, q url.Values) {
+	var action string
+	if len(q["action"]) > 0 {
+		action = q["action"][0]
+	}
+
+	client, err := xtreamapi.New(c.XtreamUser.String(), c.XtreamPassword.String(), c.XtreamBaseURL, ctx.Request.UserAgent())
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	resp, httpcode, err := client.Action(c.ProxyConfig, action, q)
+	if err != nil {
+		ctx.AbortWithError(httpcode, err) // nolint: errcheck
+		return
+	}
+
+	log.Printf("[iptv-proxy] %v | %s |Action\t%s\n", time.Now().Format("2006/01/02 - 15:04:05"), ctx.ClientIP(), action)
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	ctx.JSON(http.StatusOK, resp)
+}
+
+func (c *Config) xtreamXMLTV(ctx *gin.Context) {
+	client, err := xtreamapi.New(c.XtreamUser.String(), c.XtreamPassword.String(), c.XtreamBaseURL, ctx.Request.UserAgent())
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	resp, err := client.GetXMLTV()
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	ctx.Data(http.StatusOK, "application/xml", resp)
+}
+
+func (c *Config) xtreamStreamHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	rpURL, err := url.Parse(fmt.Sprintf("%s/%s/%s/%s", c.XtreamBaseURL, c.XtreamUser, c.XtreamPassword, id))
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.xtreamStream(ctx, rpURL)
+}
+
+func (c *Config) xtreamStreamLive(ctx *gin.Context) {
+	id := ctx.Param("id")
+	rpURL, err := url.Parse(fmt.Sprintf("%s/live/%s/%s/%s", c.XtreamBaseURL, c.XtreamUser, c.XtreamPassword, id))
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.xtreamStream(ctx, rpURL)
+}
+
+func (c *Config) xtreamStreamTimeshift(ctx *gin.Context) {
+	duration := ctx.Param("duration")
+	start := ctx.Param("start")
+	id := ctx.Param("id")
+	rpURL, err := url.Parse(fmt.Sprintf("%s/timeshift/%s/%s/%s/%s/%s", c.XtreamBaseURL, c.XtreamUser, c.XtreamPassword, duration, start, id))
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.stream(ctx, rpURL)
+}
+
+func (c *Config) xtreamStreamMovie(ctx *gin.Context) {
+	id := ctx.Param("id")
+	rpURL, err := url.Parse(fmt.Sprintf("%s/movie/%s/%s/%s", c.XtreamBaseURL, c.XtreamUser, c.XtreamPassword, id))
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.xtreamStream(ctx, rpURL)
+}
+
+func (c *Config) xtreamStreamSeries(ctx *gin.Context) {
+	id := ctx.Param("id")
+	rpURL, err := url.Parse(fmt.Sprintf("%s/series/%s/%s/%s", c.XtreamBaseURL, c.XtreamUser, c.XtreamPassword, id))
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.xtreamStream(ctx, rpURL)
+}
+
+func (c *Config) xtreamHlsStream(ctx *gin.Context) {
+	chunk := ctx.Param("chunk")
+	s := strings.Split(chunk, "_")
+	if len(s) != 2 {
+		ctx.AbortWithError( // nolint: errcheck
+			http.StatusInternalServerError,
+			errors.New("HSL malformed chunk"),
+		)
+		return
+	}
+	channel := s[0]
+
+	reqUrl, err := getHlsRedirectURL(channel)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	req, err := reqUrl.Parse(
+		fmt.Sprintf(
+			"%s://%s/hls/%s/%s",
+			reqUrl.Scheme,
+			reqUrl.Host,
+			ctx.Param("token"),
+			ctx.Param("chunk"),
+		),
+	)
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.xtreamStream(ctx, req)
+}
+
+func (c *Config) xtreamHlsrStream(ctx *gin.Context) {
+	channel := ctx.Param("channel")
+
+	reqUrl, err := getHlsRedirectURL(channel)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	req, err := reqUrl.Parse(
+		fmt.Sprintf(
+			"%s://%s/hlsr/%s/%s/%s/%s/%s/%s",
+			reqUrl.Scheme,
+			reqUrl.Host,
+			ctx.Param("token"),
+			c.XtreamUser,
+			c.XtreamPassword,
+			ctx.Param("channel"),
+			ctx.Param("hash"),
+			ctx.Param("chunk"),
+		),
+	)
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	c.xtreamStream(ctx, req)
+}
+
+func getHlsRedirectURL(channel string) (*url.URL, error) {
+	hlsChannelsRedirectURLLock.RLock()
+	defer hlsChannelsRedirectURLLock.RUnlock()
+
+	reqUrl, ok := hlsChannelsRedirectURL[channel+".m3u8"]
+	if !ok {
+		return nil, errors.New("HSL redirect reqUrl not found")
+	}
+
+	return &reqUrl, nil
+}
+
+func (c *Config) hlsXtreamStream(ctx *gin.Context, oriURL *url.URL) {
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	req, err := http.NewRequest("GET", oriURL.String(), nil)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+
+	mergeHTTPHeader(req.Header, ctx.Request.Header)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusFound {
+		location, err := resp.Location()
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+			return
+		}
+		id := ctx.Param("id")
+		if strings.Contains(location.String(), id) {
+			hlsChannelsRedirectURLLock.Lock()
+			hlsChannelsRedirectURL[id] = *location
+			hlsChannelsRedirectURLLock.Unlock()
+
+			hlsReq, err := http.NewRequest("GET", location.String(), nil)
+			if err != nil {
+				ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+				return
+			}
+
+			mergeHTTPHeader(hlsReq.Header, ctx.Request.Header)
+
+			hlsResp, err := client.Do(hlsReq)
+			if err != nil {
+				ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+				return
+			}
+			defer hlsResp.Body.Close()
+
+			b, err := ioutil.ReadAll(hlsResp.Body)
+			if err != nil {
+				ctx.AbortWithError(http.StatusInternalServerError, err) // nolint: errcheck
+				return
+			}
+			body := string(b)
+			body = strings.ReplaceAll(body, "/"+c.XtreamUser.String()+"/"+c.XtreamPassword.String()+"/", "/"+c.User.String()+"/"+c.Password.String()+"/")
+
+			mergeHTTPHeader(ctx.Writer.Header(), hlsResp.Header)
+
+			ctx.Data(http.StatusOK, hlsResp.Header.Get("Content-Type"), []byte(body))
+			return
+		}
+		ctx.AbortWithError(http.StatusInternalServerError, errors.New("Unable to hls stream")) // nolint: errcheck
+		return
+	}
+
+	ctx.Status(resp.StatusCode)
+}
